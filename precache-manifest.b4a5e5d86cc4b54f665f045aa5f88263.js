3d161bab0029945cf0efc3f1d45f3cf82bd67fdf
k3vin710 <kevinbpattani@gmail.com>
2020-02-23 11:13:32 +0530

    Updates

---
+++ precache-manifest.b4a5e5d86cc4b54f665f045aa5f88263.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe9ac263f34a1c46f83a7e04aacd6dc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8affe4c88482626b0231",
+    "url": "/monsters-rolodex/static/css/main.48e88e06.chunk.css"
+  },
+  {
+    "revision": "c68e713413129a1341ab",
+    "url": "/monsters-rolodex/static/js/2.2490ac36.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.2490ac36.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8affe4c88482626b0231",
+    "url": "/monsters-rolodex/static/js/main.ff367454.chunk.js"
+  },
+  {
+    "revision": "b1fefa23703c0839d401",
+    "url": "/monsters-rolodex/static/js/runtime-main.3ec433b8.js"
+  }
+]);
